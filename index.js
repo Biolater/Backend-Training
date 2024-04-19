5f249fac400737aa029dad75946110ef48a206b0
Murad <yusifovmurad1@gmail.com>
2024-04-19 14:37:28 +0400

    Middlewares

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
-import express from "express";
+import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send(`<h1>Hello welcome port is ${port}</h1>`);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('<h1>YOOO</h1>')
 });
 
-app.get("/about", (req, res) => {
-  res.send("<h1>About</h1>");
+app.post('/submit', (req, res) => {
+  console.log(req.body)
 });
-
-app.post("/register", (req, res) => {
-  res.status(201).send("<h1>Register</h1>");
-});
-
-app.put("/user/Murad", (req, res) => {
-  res.status(200).send("<h1>User Murad Added</h1>");
-});
-
-app.patch("/user/Murad", (req, res) => {
-  res.status(200).send("<h1>User Murad patched</h1>");
-});
-
-app.delete("/user/Murad", (req, res) => {
-  res.status(200).send("<h1>User Murad deleted</h1>");
-});
-
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`)
 });
