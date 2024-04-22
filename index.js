79103a9e996b186dcbb58df14d1438b44886bce8
Murad <yusifovmurad1@gmail.com>
2024-04-22 18:28:32 +0400

    ejs

--- index.js
+++ index.js
@@ -4,29 +4,19 @@ import { dirname } from 'path'
 import { fileURLToPath } from 'url';
 
 const ___dirname = dirname(fileURLToPath(import.meta.url));
-
+const port = 3000;
 const app = express();
-let bandname = '';
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-function bandNameGenerator(req, res, next) {
-    console.log(req.body);
-    bandname = req.body['street'] + req.body['pet'];
-    next();
-}
-
-app.use(bandNameGenerator);
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.sendFile(___dirname + '/index.html');
-})
+    res.render('index.ejs', {
+        dayType: 'a weekday',
+        advice: "Don't forget to eat healthy",
+    });
+});
 
-app.post('/submit', (req, res) => {
-    res.send(`<h1>Your band name is ${bandname}</h1>`)
-})
 
-
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
